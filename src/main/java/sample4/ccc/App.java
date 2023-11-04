ae24adefd8f60eacfda003fa312d53a83a8f51ef
shari <shari@LAPTOP-H8J5G4BF>
2023-11-04 09:17:38 +0530

    this is a commit 1

---
+++ src/main/java/sample4/ccc/App.java
@@ -0,0 +1,13 @@
+package sample4.ccc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello BEC this is a wonderfull morning....!!!!" );
+    }
+}
